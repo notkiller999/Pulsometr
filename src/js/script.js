99fd10e430dbffd1670a3707a15a0ff698acde26
Vladyslav <notkiller999@gmail.com>
2021-01-23 21:51:45 +0200

    Tabs (part 1)
    make tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,3 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1000,
-//         slidesToShow: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/next.png"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     arrows: false,
-//                     dots: true
-//                 }
-//             }
-//         ]
-//     });
-//   });
-
 
 const slider = tns({
     container: '.carousel__inner',
